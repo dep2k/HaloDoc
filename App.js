498f8398507c11d4165fafaa0b64e774de44cca4
Priya <priya.mobdev@gmail.com>
2018-09-05 21:37:35 +0530

    Sample pages

--- App.js
+++ App.js
@@ -4,12 +4,20 @@ import { Provider } from "react-redux";
 import AppStackNavigator from './src/Navigation/AppNavigator'
 import store from './src/store'
 
-// import AWSAppSyncClient from "aws-appsync";
-// import { Rehydrated } from "aws-appsync-react";
-// import { AUTH_TYPE } from "aws-appsync/lib/link/auth-link";
-// import { graphql, ApolloProvider, compose } from "react-apollo";
-// import * as AWS from "aws-sdk";
-// import AppSync from "./AppSync.js";
+import AWSAppSyncClient from "aws-appsync";
+import { Rehydrated } from "aws-appsync-react";
+import { AUTH_TYPE } from "aws-appsync/lib/link/auth-link";
+import { graphql, ApolloProvider, compose } from "react-apollo";
+import * as AWS from "aws-sdk";
+
+// const client = new AWSAppSyncClient({
+//   url: awsconfig.graphqlEndpoint,
+//   region: awsconfig.region,
+//   auth: { type: AUTH_TYPE.API_KEY, apiKey: awsconfig.apiKey }
+// });
+
+
+//import AppSync from "./AppSync.js";
 // import AllPostsQuery from "./Queries/AllPostsQuery";
 // import NewPostMutation from "./Queries/NewPostMutation";
 // import DeletePostMutation from "./Queries/DeletePostMutation";
@@ -22,8 +30,9 @@ export default class App extends React.Component {
       <Provider store={store}>
         <AppStackNavigator />
       </Provider>
-
     );
+     
+   
   }
 }
 
--- src/Navigation/AppNavigator.js
+++ src/Navigation/AppNavigator.js
@@ -1,20 +1,23 @@
 
 import LoginPage from '../components/LoginAndSignup/LoginPage'
 import MainMenuPage from '../components/MainMenuPage'
-import  TestPage from '../components/TestPage'
-import TextInputUI from '../components/TextInputUI'
-import TestPageUI from '../components/TestPageUI'
-import ButtonUI from '../components/ButtonUI'
-
+// import  TestPage from '../components/TestPage'
+// import TextInputUI from '../components/TextInputUI'
+// import TestPageUI from '../components/TestPageUI'
+// import ButtonUI from '../components/ButtonUI'
+ import TestLoginPage from '../components/TestLoginPage'
+// import TestFlatList from '../components/TestFlatList'
 
 import { createStackNavigator } from 'react-navigation'
 
 
 const AppStackNavigator = createStackNavigator({
-    ButtonUI: ButtonUI,
-    TextInputUI: TextInputUI,
-    TestPageUI : TestPageUI,
-    Test: TestPage,
+    // ButtonUI: ButtonUI,
+    // TestFlatList: TestFlatList,
+     TestLoginPage : TestLoginPage,
+    // TextInputUI: TextInputUI,
+    // TestPageUI : TestPageUI,
+    // Test: TestPage,
     Login: LoginPage,
     Menu: MainMenuPage,
 
