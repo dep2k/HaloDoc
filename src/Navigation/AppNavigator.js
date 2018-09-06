5e68e0be8faac6e69c9ad62e3f3b7b6779a8894b
Priya <priya.mobdev@gmail.com>
2018-09-06 08:50:28 +0530

    Chat Module Added

--- src/Navigation/AppNavigator.js
+++ src/Navigation/AppNavigator.js
@@ -5,13 +5,15 @@ import  TestPage from '../components/TestPage'
 import TextInputUI from '../components/TextInputUI'
 import TestPageUI from '../components/TestPageUI'
 import ButtonUI from '../components/ButtonUI'
-
-
+import UserVetChat from '../components/Consultation/UserVetChat'
+import TestLoginPage from '../components/TestLoginPage'
 import { createStackNavigator } from 'react-navigation'
 
 
 const AppStackNavigator = createStackNavigator({
+    TestLoginPage: TestLoginPage,
     ButtonUI: ButtonUI,
+    Chat: UserVetChat,
     TextInputUI: TextInputUI,
     TestPageUI : TestPageUI,
     Test: TestPage,
