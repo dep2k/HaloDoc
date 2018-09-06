d726a91fcd39c65cede926a27ba3907e11d6d8f2
Priya <priya.mobdev@gmail.com>
2018-09-06 21:46:08 +0530

    Navigator Updated, header removed

--- src/Navigation/AppNavigator.js
+++ src/Navigation/AppNavigator.js
@@ -1,26 +1,52 @@
 
 import LoginPage from '../components/LoginAndSignup/LoginPage'
 import MainMenuPage from '../components/MainMenuPage'
+import TestLoginPage from '../components/TestLoginPage'
+import PetCategories from '../components/PetRegistration/PetCategories'
+import { createStackNavigator } from 'react-navigation'
+
 // import  TestPage from '../components/TestPage'
 // import TextInputUI from '../components/TextInputUI'
 // import TestPageUI from '../components/TestPageUI'
 // import ButtonUI from '../components/ButtonUI'
- import TestLoginPage from '../components/TestLoginPage'
 // import TestFlatList from '../components/TestFlatList'
 
-import { createStackNavigator } from 'react-navigation'
+const AppStackNavigator = createStackNavigator(
+  {
+    PetCategories: {
+      screen: PetCategories,
+      navigationOptions: {
+        header: null
+      }
+    },
 
+    TestLoginPage: {
+      screen: TestLoginPage
+    },
 
-const AppStackNavigator = createStackNavigator({
+    Login: {
+      screen: LoginPage,
+      navigationOptions: ({ navigation }) => ({
+        title: "Home",
+        headerStyle: styles.headerStyle,
+        headerTitle: <Text>Home</Text>,
+        headerLeft: null,
+        headerRight: null
+      })
+    },
+
+    Menu: {
+        screen: MainMenuPage
+    },
     // ButtonUI: ButtonUI,
     // TestFlatList: TestFlatList,
-     TestLoginPage : TestLoginPage,
     // TextInputUI: TextInputUI,
     // TestPageUI : TestPageUI,
     // Test: TestPage,
-    Login: LoginPage,
-    Menu: MainMenuPage,
-
-})
+  },
+  {
+    initialRouteName: "PetCategories"
+  }
+);
 
 export default AppStackNavigator;
