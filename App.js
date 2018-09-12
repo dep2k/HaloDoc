0a4a7a4ebd25e41c560c590abce02433e72cf1d9
Priya <priya.mobdev@gmail.com>
2018-09-12 09:42:53 +0530

    Merge App.js

--- App.js
+++ App.js
@@ -9,6 +9,9 @@ import { Rehydrated } from "aws-appsync-react";
 import { AUTH_TYPE } from "aws-appsync/lib/link/auth-link";
 import { graphql, ApolloProvider, compose } from "react-apollo";
 import * as AWS from "aws-sdk";
+import AppSync from "./AppSync.js";
+
+
 
 // const client = new AWSAppSyncClient({
 //   url: awsconfig.graphqlEndpoint,
@@ -23,16 +26,41 @@ import * as AWS from "aws-sdk";
 // import DeletePostMutation from "./Queries/DeletePostMutation";
 // import UpdatePostMutation from "./Queries/UpdatePostMutation";
 
+const client = new AWSAppSyncClient({
+  url: AppSync.graphqlEndpoint,
+  region: AppSync.region,
+  auth: {
+    type: AUTH_TYPE.API_KEY,
+    apiKey: AppSync.apiKey,
+
+    //type: AUTH_TYPE.AWS_IAM,
+    //Note - Testing purposes only
+    /*credentials: new AWS.Credentials({
+        accessKeyId: AWS_ACCESS_KEY_ID,
+        secretAccessKey: AWS_SECRET_ACCESS_KEY
+    })*/
+
+    //IAM Cognito Identity using AWS Amplify
+    //credentials: () => Auth.currentCredentials(),
+
+    //Cognito User Pools using AWS Amplify
+    // type: AUTH_TYPE.AMAZON_COGNITO_USER_POOLS,
+    // jwtToken: async () => (await Auth.currentSession()).getIdToken().getJwtToken(),
+  },
+});
+
 export default class App extends React.Component {
 
   render() {
     return (
-      <Provider store={store}>
-        <AppStackNavigator />
-      </Provider>
+      <ApolloProvider client={client}>
+        <Rehydrated>
+          <AppStackNavigator />
+        </Rehydrated>
+      </ApolloProvider>
+
     );
      
-   
   }
 }
 
