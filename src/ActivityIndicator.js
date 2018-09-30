e95429996f5e031ae4a4be5d6bd4456048a7efc5
Priya <priya.mobdev@gmail.com>
2018-09-30 23:13:35 +0530

    src/ActivityIndicator.js

--- src/ActivityIndicator.js
+++ src/ActivityIndicator.js
@@ -8,31 +8,18 @@ import {
 export default Loader = (props) => {
 
     return (
-<<<<<<< HEAD
     
         <ActivityIndicator animating={props.animating}
             style = {styles.activityIndicator}
             size="large"
             color="black"
         /> 
-=======
-        
-        <ActivityIndicator animating={props.animating}
-                            style={styles.activityIndicator}
-                            size="large"
-                            color = "black"
-        />
->>>>>>> 1c8c59886f39b7d17296059b6468df173cffcc79
     );
 
 
 }
 const styles = StyleSheet.create({
-<<<<<<< HEAD
    
-=======
-    
->>>>>>> 1c8c59886f39b7d17296059b6468df173cffcc79
     activityIndicator: {
         position: 'absolute',
         left: 0,
