33082dfbc399d5691bae36396e5e5c81f14e8d36
deep <deep.arora@redmart.com>
2019-02-04 23:56:36 +0530

    FeedbackPage Title issue fix

--- src/components/Consultation/FeedbackPage.js
+++ src/components/Consultation/FeedbackPage.js
@@ -1,138 +1,100 @@
 import React from "react";
 import {
-    View,
-    Image,
-    StyleSheet,
-    Text,
-    TouchableOpacity,
-    ImageBackground,
-    FlatList,
-    Button,
-    ActivityIndicator
+  View,
+  Image,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  ImageBackground,
+  FlatList,
+  Button,
+  ActivityIndicator
 } from "react-native";
 import { I18n } from "aws-amplify";
-
 
 import { logoImage, petWithPathImage } from "../../images/resource";
 
 import { NavBar } from "../Reusable/NavBar";
 import { Footer } from "../Reusable/Footer";
 
-
 class FeedbackPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.continueBtnClick = this.continueBtnClick.bind(this);
+    this.backButtonClick = this.backButtonClick.bind(this);
+  }
 
-    constructor(props) {
+  continueBtnClick() {
+    //  const { navigation } = this.props;
+    //  const pet = navigation.getParam('petInfo');
+    //  this.props.navigation.navigate("QuestionsPage",{
+    //      petInfo:pet,
+    //  });
+  }
 
-        super(props);      
-        this.continueBtnClick = this.continueBtnClick.bind(this);
-        this.backButtonClick = this.backButtonClick.bind(this);
-    }
+  cancelBtnClick() {
+    this.props.navigation.navigate("MainMenuPage");
+  }
 
-    continueBtnClick() {
-         
-        //  const { navigation } = this.props;
-        //  const pet = navigation.getParam('petInfo');
-        //  this.props.navigation.navigate("QuestionsPage",{
-        //      petInfo:pet,
-        //  });
-    }
+  backButtonClick() {
+    this.props.navigation.navigate("MainMenuPage");
+  }
 
-    cancelBtnClick() {
-        this.props.navigation.navigate("MainMenuPage");
-    }
-
-
-    backButtonClick() {
-        this.props.navigation.navigate("MainMenuPage");
-    }
-
-  
-
-    render() {
-
-
-        // const { navigation } = this.props;
-        // const pet = navigation.getParam('petInfo');
-        // const petName = pet.name;
-        // const petCategory = pet.category;
-        // const navTitle = petName + " - " + petCategory;
-  
-
-        return (
-            <View style={styles.mainContainer}>
-
-                <NavBar onBackPress={this.backButtonClick} title = {""}></NavBar>
-
-                <View style = {styles.contentView}>
-                   
-                    <View style={styles.descriptionView}>
-                         <ImageBackground
-                             source={petWithPathImage}
-                             style= { styles.petWithPathStyle}
-                             imageStyle={styles.petWithPathImageStyle}>
-                
-                            <Text style={styles.descriptionText}
-                                    numberOfLines={0}>{ I18n.get("ConsultationEndMessage")}
-                            </Text>
-                        </ImageBackground>   
-                    </View>
-
-                </View>
-              
-
-                <Footer style = {styles.footer} title = " " hideBtn = {true} onPress = {this.continueBtnClick}></Footer>
-  
-
+  render() {
+    return (
+      <View style={styles.mainContainer}>
+        <NavBar onBackPress={this.backButtonClick} title={" "} />
+        <View style={styles.contentView}>
+          <View style={styles.descriptionView}>
+          
+              <Text style={styles.descriptionText} numberOfLines={0}>
+                {I18n.get("ConsultationEndMessage")}
+              </Text>
+        
+          </View>
         </View>
-
-        );
-    }
-           
+        <Footer
+          style={styles.footer}
+          title=" "
+          hideBtn={true}
+          onPress={this.continueBtnClick}
+        />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
+  mainContainer: {
+    flex: 1
+  },
 
-    mainContainer: {
-        flex: 1,
-        
-    },
+  contentView: {
+    //height: "70%",
+    flex: 2.3
+  },
 
-    contentView: {
-        //height: "70%",
-        flex:2.3
-    },
+  descriptionView: {
+    height: "100%",
+    alignItems: "center"
+  },
 
+  petWithPathImageStyle: {
+    resizeMode: "contain"
+  },
 
-    descriptionView: {
-      
-        height: "100%",
-        alignItems: "center",
-     
-        
-    },
+  petWithPathStyle: {
+    height: "100%",
+    width: "80%"
+  },
 
-    petWithPathImageStyle: {
-        
-        resizeMode: "contain",
-    },
- 
-    petWithPathStyle: {
-        height: "100%",
-        width: "80%",  
-    },
-
-    descriptionText: {
-        
-        marginTop: 200,
-        marginLeft: 10,
-        marginRight: 10,
-        fontSize: 24,
-        textAlign: 'center'
-        
-    },
-
-  
-})
-
+  descriptionText: {
+    marginTop: 200,
+    marginLeft: 10,
+    marginRight: 10,
+    fontSize: 24,
+    textAlign: "center"
+  }
+});
 
 export default FeedbackPage;
